252884d25a299c227e3aed41d9e21196ed5b0f33
Alxandr <alxandr@alxandr.me>
2011-09-30 01:09:22 +0200

    Added logger to Utils.

---
+++ src/AddIns/Utils/Logger.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Linq;
+using Jurassic;
+using Jurassic.Library;
+using NLog;
+using RunJS.Core;
+
+namespace RunJS.AddIn.Utils
+{
+    /// <summary>
+    /// A logger-constructor used in javascript to construct loggers.
+    /// </summary>
+    [JsAddIn("logger")]
+    public class LoggerConstructor : ClrFunction
+    {
+        private ScriptRunner runner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggerConstructor"/> class.
+        /// </summary>
+        /// <param name="runner">The script runner.</param>
+        public LoggerConstructor(ScriptRunner runner)
+            : base(runner.Engine.Function.Prototype, "Log", runner.Engine.Object.InstancePrototype)
+        {
+            this.runner = runner;
+        }
+
+        //     JAVASCRIPT INTERNAL FUNCTIONS
+        //_________________________________________________________________________________________
+
+        /// <summary>
+        /// Constructs the specified logger.
+        /// </summary>
+        /// <param name="logName">Name of the log.</param>
+        /// <returns>A new LoggerInstance.</returns>
+        [JSConstructorFunction]
+        public LoggerInstance Construct(string logName)
+        {
+            if (logName == "null" || logName == "default")
+                logName = null;
+            if (String.IsNullOrWhiteSpace(logName))
+                throw new JavaScriptException(Engine, "Error", "logName can't be empty");
+            return new LoggerInstance(this.InstancePrototype, this.runner, logName);
+        }
+    }
+
+    /// <summary>
+    /// A logger-instance used by javascript.
+    /// </summary>
+    public class LoggerInstance : ObjectInstance
+    {
+        private static Logger classLogger = LogManager.GetCurrentClassLogger();
+
+        private ScriptRunner scriptRunner;
+        private string logName;
+        private Logger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggerInstance"/> class.
+        /// </summary>
+        /// <param name="prototype">The prototype.</param>
+        /// <param name="scriptRunner">The script runner.</param>
+        /// <param name="logName">Name of the log.</param>
+        public LoggerInstance(ObjectInstance prototype, ScriptRunner scriptRunner, string logName)
+            : base(prototype)
+        {
+            this.scriptRunner = scriptRunner;
+            logName = logName[0].ToString().ToUpper() + logName.Substring(1);
+            this.logName = logName;
+            this.logger = LogManager.GetLogger("JSLogger." + logName);
+
+            classLogger.Trace("Logger {0} initialized.", logName);
+            PopulateFunctions();
+        }
+
+        /// <summary>
+        /// Traces the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The args.</param>
+        [JSFunction(Name = "trace")]
+        public void Trace(string message, params object[] args)
+        {
+            logger.Trace(message, args.JsonEntype(Engine));
+        }
+
+        /// <summary>
+        /// Debugs the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The args.</param>
+        [JSFunction(Name = "debug")]
+        public void Debug(string message, params object[] args)
+        {
+            logger.Debug(message, args.JsonEntype(Engine));
+        }
+
+        /// <summary>
+        /// Infoes the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The args.</param>
+        [JSFunction(Name = "info")]
+        public void Info(string message, params object[] args)
+        {
+            logger.Info(message, args.JsonEntype(Engine));
+        }
+
+        /// <summary>
+        /// Warns the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The args.</param>
+        [JSFunction(Name = "warn")]
+        public void Warn(string message, params object[] args)
+        {
+            logger.Warn(message, args.JsonEntype(Engine));
+        }
+
+        /// <summary>
+        /// Errors the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The args.</param>
+        [JSFunction(Name = "error")]
+        public void Error(string message, params object[] args)
+        {
+            logger.Error(message, args.JsonEntype(Engine));
+        }
+
+        /// <summary>
+        /// Fatals the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The args.</param>
+        [JSFunction(Name = "fatal")]
+        public void Fatal(string message, params object[] args)
+        {
+            logger.Fatal(message, args.JsonEntype(Engine));
+        }
+    }
+
+    static class LoggerExtensions
+    {
+        public static object[] JsonEntype(this object[] array, ScriptEngine engine)
+        {
+            return array.Select(itm =>
+            {
+                if (itm is ObjectInstance)
+                {
+                    try
+                    {
+                        return JSONObject.Stringify(engine, itm).Trim();
+                    }
+                    catch
+                    { }
+                }
+                return itm.ToString();
+            }).ToArray();
+        }
+    }
+}
---
+++ tests/AddIns/Utils/LoggerTests.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NUnit.Framework;
+using RunJS.Core;
+using Should.Fluent;
+
+namespace RunJS.AddIn.Utils.Tests
+{
+    [TestFixture]
+    public class LoggerTests
+    {
+        private List<LogEventInfo> logs;
+        private LoggingConfiguration oldConfig;
+        private ScriptRunner scriptRunner;
+        private class TestLoggerTarget : Target
+        {
+            private List<LogEventInfo> logs = new List<LogEventInfo>();
+            protected override void Write(LogEventInfo logEvent)
+            {
+                logs.Add(logEvent);
+            }
+
+            public List<LogEventInfo> Logs
+            {
+                get { return logs; }
+            }
+        }
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            var target = new TestLoggerTarget();
+            logs = target.Logs;
+
+            var config = new LoggingConfiguration();
+            config.AddTarget("fake", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Trace, target));
+            oldConfig = LogManager.Configuration;
+            LogManager.Configuration = config;
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            LogManager.Configuration = oldConfig;
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            scriptRunner = new ScriptRunner();
+            scriptRunner.Run();
+            scriptRunner.Execute("this.Logger = require('utils', 'logger');");
+            logs.Clear();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            scriptRunner.Dispose();
+        }
+
+        [Test]
+        public void CreatingALoggerCreatesALog()
+        {
+            scriptRunner.Execute("var logger = new Logger('test');");
+            logs.Count.Should().Equal(1);
+        }
+
+        [Test]
+        public void LogLevelsTest()
+        {
+            scriptRunner.Execute("var logger = new Logger('test');");
+            scriptRunner.Execute("logger.trace('test');");
+            scriptRunner.Execute("logger.debug('test');");
+            scriptRunner.Execute("logger.info('test');");
+            scriptRunner.Execute("logger.warn('test');");
+            scriptRunner.Execute("logger.error('test');");
+            scriptRunner.Execute("logger.fatal('test');");
+
+            logs.Count.Should().Equal(7);
+            logs[1].Level.Should().Equal(LogLevel.Trace);
+            logs[2].Level.Should().Equal(LogLevel.Debug);
+            logs[3].Level.Should().Equal(LogLevel.Info);
+            logs[4].Level.Should().Equal(LogLevel.Warn);
+            logs[5].Level.Should().Equal(LogLevel.Error);
+            logs[6].Level.Should().Equal(LogLevel.Fatal);
+        }
+
+        [Test]
+        public void LoggerNameTests()
+        {
+            scriptRunner.Execute("var logger1 = new Logger('test');");
+            scriptRunner.Execute("var logger2 = new Logger('Test');");
+            scriptRunner.Execute("var logger3 = new Logger('knut');");
+            scriptRunner.Execute("var logger4 = new Logger('Paal');");
+            scriptRunner.Execute("var logger5 = new Logger('olE');");
+            logs.Clear();
+
+            scriptRunner.Execute("logger1.trace('test');");
+            scriptRunner.Execute("logger2.trace('test');");
+            scriptRunner.Execute("logger3.trace('test');");
+            scriptRunner.Execute("logger4.trace('test');");
+            scriptRunner.Execute("logger5.trace('test');");
+
+            logs.Count.Should().Equal(5);
+            logs[0].LoggerName.Should().Equal("JSLogger.Test");
+            logs[1].LoggerName.Should().Equal("JSLogger.Test");
+            logs[2].LoggerName.Should().Equal("JSLogger.Knut");
+            logs[3].LoggerName.Should().Equal("JSLogger.Paal");
+            logs[4].LoggerName.Should().Equal("JSLogger.OlE");
+        }
+
+        [Test]
+        public void LogMessageTests()
+        {
+            scriptRunner.Execute("var logger = new Logger('test');");
+            scriptRunner.Execute("logger.trace('test1');");
+            scriptRunner.Execute("logger.trace('test2');");
+            scriptRunner.Execute("logger.trace('test3');");
+            scriptRunner.Execute("logger.trace('test4');");
+            scriptRunner.Execute("logger.trace('test5');");
+
+            logs.Count.Should().Equal(6);
+            for (int i = 1; i < 6; i++)
+                logs[i].Message.Should().Equal("test" + i);
+        }
+
+        [Test]
+        public void LogParameters()
+        {
+            scriptRunner.Execute("var logger = new Logger('test');");
+            scriptRunner.Execute("logger.trace('test');");
+            scriptRunner.Execute("logger.trace('test', 1);");
+            scriptRunner.Execute("logger.trace('test', 1, 2, 3);");
+            scriptRunner.Execute("logger.trace('test', {age:5});");
+
+            logs.Count.Should().Equal(5);
+
+            logs[1].Parameters.Length.Should().Equal(0);
+
+            logs[2].Parameters.Length.Should().Equal(1);
+            logs[2].Parameters[0].Should().Equal("1");
+
+            logs[3].Parameters.Length.Should().Equal(3);
+            logs[3].Parameters[0].Should().Equal("1");
+            logs[3].Parameters[1].Should().Equal("2");
+            logs[3].Parameters[2].Should().Equal("3");
+
+            logs[4].Parameters.Length.Should().Equal(1);
+            logs[4].Parameters[0].Should().Equal("{\"age\":5}");
+        }
+    }
+}
